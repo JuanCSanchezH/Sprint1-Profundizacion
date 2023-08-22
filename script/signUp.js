39cc8c83758267d7412463c4e9abc92f72dae575
Juan Camilo Sánchez <juan.sanchez53@udea.edu.co>
2023-08-21 20:23:59 -0500

    SignIn & SignUp UI

---
+++ script/signUp.js
@@ -0,0 +1,7 @@
+import Swal from '../node_modules/sweetalert2/src/sweetalert2.js'
+Swal.fire({
+    title: 'Error!',
+    text: 'Do you want to continue',
+    icon: 'success',
+    confirmButtonText: 'Cool'
+})
